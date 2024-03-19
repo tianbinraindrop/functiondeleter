cde78408c06d0bbe4a01b53206179dd8a52952d5
tianbinraindrop <tianbinraindrop@163.com>
2024-03-19 22:15:48 +0800

    fix dependency

---
+++ sanbox/ast-sandbox.py
@@ -0,0 +1,108 @@
+from pandas import DataFrame
+from connectioneditor.action_db import *
+import pandas
+
+
+class action_demo1():
+
+    def __init__(self) -> None:
+        self.pg = con_postgresql()
+        self.conn = self.pg.connect()
+        self.tablename = 'vimtips'
+
+
+class action_demo3():
+
+    class nested_class():
+
+        def add(a, b):
+            return a + b
+
+    def fundemo():
+        print('here is me')
+
+
+class action_vim():
+
+    def __init__(self) -> None:
+        self.pg = con_postgresql()
+        self.conn = self.pg.connect()
+        self.tablename = 'vimtips'
+
+    def add(self, operation: str, mode: str, frequency: str, hard: str,
+            vscode: bool, instructions: str, memo: str, actiontype: str):
+        """
+        增加一条vim记录
+        """
+        # sql语句不认识'，所以要替换成"
+        memo = memo.replace("'", '"')
+        instructions = instructions.replace("'", '"')
+        operation = operation.strip()
+        instructions = instructions.strip()
+        memo = memo.strip()
+
+        sql = f"""insert into {self.tablename} (operation,mode,frequency,hard,vscode,instructions,memo,actiontype) values ('{operation}','{mode}','{frequency}','{hard}','{vscode}','{instructions}','{memo}','{actiontype}') """
+        self.pg.execute(sql)
+
+    def update(self, id: str, operation: str, mode: str, frequency: str,
+               hard: str, vscode: bool, instructions: str, memo: str,
+               actiontype: str):
+        """
+        修改一条vim记录
+
+        Args:
+        """
+        sql = f"""update {self.tablename} set operation = '{operation}', mode ='{mode}',frequency='{frequency}',hard='{hard}',vscode='{vscode}',instructions='{instructions}',memo='{memo}',actiontype='{actiontype}' where id={id} """
+        self.pg.execute(sql)
+
+    def all(self, orderby: str = "id", OrderDirect: bool = True) -> DataFrame:
+        """
+        排序返回所有数据记录
+        Args:
+            orderby (str): 排序字段
+            OrderDirect (bool): 是否降序
+        Returns:
+            DataFrame: 返回所有数据记录
+        """
+        if OrderDirect:
+            sqlstr = f"""select id,operation,mode,frequency,hard,vscode,instructions,memo,actiontype from {self.tablename} order by {orderby} desc """
+        else:
+            sqlstr = f"""select id,operation,mode,frequency,hard,vscode,instructions,memo,actiontype from {self.tablename} order by {orderby} """
+
+        return self.pg.query(sqlstr)
+
+    def query(self, sql: str) -> DataFrame:
+        """
+        查询数据
+
+        Args:
+            sql (str): sql语句
+
+        Returns:
+            DataFrame: 结果集
+        """
+        return self.pg.query(sql)
+
+    def remove(self, colname: str, value: str):
+        """
+        删除指定的记录
+
+        Args:
+            colname (str): [列名]
+            value (str): [判断的值]
+        """
+        sqlstr = f"""delete from {self.tablename} where {colname } = '{value}' """
+        self.pg.execute(sqlstr)
+
+    def get(self, colname: str, value: str) -> tuple:
+        """
+        获得指定的记录
+
+        Args:
+            colname (str): [列名]
+            value (str): [判断的值]
+        Returns:
+            tuple: 返回记录
+        """
+        sqlstr = f"""select id,operation,mode,frequency,hard,vscode,instructions,memo,actiontype from {self.tablename} where {colname } = '{value}' """
+        return self.pg.fetch(sqlstr)
---
+++ sanbox/reset.ps1
@@ -0,0 +1,1 @@
+copy ./ast-sandbox-old.py ./ast-sandbox.py
